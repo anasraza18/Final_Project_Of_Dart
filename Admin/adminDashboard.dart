e27f73e0cd3b353470930e538094b66b31a18695
anasraza18 <manasraza18@gmail.com>
2025-01-12 11:11:33 +0500

    Final Project Of dart

--- Admin/adminDashboard.dart
+++ Admin/adminDashboard.dart
@@ -5,33 +5,31 @@ import 'classManagement.dart';
 import 'resultReport.dart';
 import 'userManagement.dart';
 
-adminDashboard(){
-
-
+adminDashboard() {
   do {
-  print('=============Main Menu For Admin Dashboard===============');
-  print("Option 1: User Management");
-  print("Option 2: Class Management");
-  print("Option 3: Attendance Report");
-  print("Option 4: Result Report");
-  print('');
+    print('=============Main Menu For Admin Dashboard===============');
+    print("Option 1: User Management");
+    print("Option 2: Class Management");
+    print("Option 3: Attendance Report");
+    print("Option 4: Result Report");
+    print('');
 
     stdout.write('Select Any one or exit : ');
-  String? InputOptionAdmin = stdin.readLineSync();
+    String? InputOptionAdmin = stdin.readLineSync();
 
-  if(InputOptionAdmin.toString().toLowerCase() == 'exit'){
-    break;
-  }
-  if (InputOptionAdmin == '1') {
-     userManagement();
-  }else if(InputOptionAdmin == '2'){
-     classManagement(); 
-  }else if(InputOptionAdmin == '3'){
-    attendanceReport();
-  }else if(InputOptionAdmin == '3'){
-    resultReport();
-  }else{
-    print("Invalid Option! please try again.");
-  } 
+    if (InputOptionAdmin.toString().toLowerCase() == 'exit') {
+      break;
+    }
+    if (InputOptionAdmin == '1') {
+      userManagement();
+    } else if (InputOptionAdmin == '2') {
+      classManagement();
+    } else if (InputOptionAdmin == '3') {
+      attendanceReport();
+    } else if (InputOptionAdmin == '3') {
+      resultReport();
+    } else {
+      print("Invalid Option! please try again.");
+    }
   } while (true);
 }
