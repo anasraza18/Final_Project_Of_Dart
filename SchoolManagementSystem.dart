93751ca799648cd635b33f06a3cbe762d9c413e6
anasraza18 <manasraza18@gmail.com>
2025-01-19 13:01:07 +0500

    Final Project Of dart

--- SchoolManagementSystem.dart
+++ SchoolManagementSystem.dart
@@ -1,32 +1,31 @@
 import 'dart:io';
 import 'authentication.dart';
 
-loginOption(){
-
+loginOption() {
   do {
-  print('=============Main Menu===============');
-  print("Option 1: Admin");
-  print("Option 2: Teacher");
-  print("Option 3: Student");
-  print("Option 4: Parent");
-  print('');
+    print('=============Main Menu===============');
+    print("Option 1: Admin");
+    print("Option 2: Teacher");
+    print("Option 3: Student");
+    print("Option 4: Parent");
+    print('');
 
     stdout.write('Select Any one or exit : ');
-  String? InputOption = stdin.readLineSync();
+    String? InputOption = stdin.readLineSync();
 
-  if(InputOption.toString().toLowerCase() == 'exit'){
-    break;
-  }
-  if (InputOption == '1') {
-    adminLogin(username: 'anas', password: "anas123"); 
-  }else if(InputOption == '2'){
-   teacherLogin();
-  }else if(InputOption == '3'){
-    studentLogin();
-  }else if(InputOption == '4'){
-    parentsLogin();
-  }else{
-    print("Invalid Option! please try again.");
-  }
+    if (InputOption.toString().toLowerCase() == 'exit') {
+      break;
+    }
+    if (InputOption == '1') {
+      adminLogin(username: 'anas', password: "anas123");
+    } else if (InputOption == '2') {
+      teacherLogin();
+    } else if (InputOption == '3') {
+      studentLogin();
+    } else if (InputOption == '4') {
+      parentsLogin();
+    } else {
+      print("Invalid Option! please try again.");
+    }
   } while (true);
 }
